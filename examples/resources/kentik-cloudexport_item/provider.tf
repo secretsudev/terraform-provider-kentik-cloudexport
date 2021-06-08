eb5c23924f42f7af23713348aefb0dfabfab49f8
secretsudev <secretsudev@gmail.com>
2021-06-07 17:00:11 -0700

    Fix required version of Kentik provide in demo configs

--- examples/resources/kentik-cloudexport_item/provider.tf
+++ examples/resources/kentik-cloudexport_item/provider.tf
@@ -2,7 +2,7 @@
 terraform {
   required_providers {
     kentik-cloudexport = {
-      version = "0.1.0"
+      version = "=> 0.2.0"
       source  = "kentik.com/automation/kentik-cloudexport"
     }
   }
