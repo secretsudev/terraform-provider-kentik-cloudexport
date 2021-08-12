0319186e9eaf4f6d8aa8851f3384bb2704c89b71
secretsudev <secretsudev@gmail.com>
2021-08-12 14:55:47 +0200

    Changing provider address for debug mode in main.go

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	opts := &plugin.ServeOpts{ProviderFunc: provider.New}
 
 	if debugMode {
-		err := plugin.Debug(context.Background(), "registry.terraform.io/providers/kentik/kentik-cloudexport", opts)
+		err := plugin.Debug(context.Background(), "kentik/kentik-cloudexport", opts)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
